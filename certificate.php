45c560bb5a65ac0919b89a297ddc31c3e08b1590
Akt1mel <Kostyavas.94@mail.ru>
2018-09-06 10:03:08 +0300

    Внесены поправки

--- certificate.php
+++ certificate.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 $testName = $_POST['test-name'];
 $test = json_decode(file_get_contents($testName), true);
 $result = 0;
@@ -14,12 +12,15 @@ foreach ($test as $key => $value) {
 
 $userName = $_POST["userName"];
 
-
 header("Content-type: image/png");
+
 $im = @imagecreate(600, 400)
+
 or die("Невозможно создать поток изображения");
 $background_color = imagecolorallocate($im, 102, 102, 102);
 $text_color = imagecolorallocate($im, 255, 255, 255);
-imagestring($im, 5, 5, 5,  "Your name: ".$userName." Your result: ".$result , $text_color);
+imagestring($im, 5, 210, 150,  "Your name: ".$userName, $text_color);
+imagestring($im, 5, 210, 200,  "Your result: ".$result , $text_color);
+
 imagepng($im);
 imagedestroy($im);
